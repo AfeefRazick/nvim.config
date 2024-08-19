17bed1684166e6a05c9bc5c5cf6356faf917a50d
Afeef Razick <afeefrazickamir@gmail.com>
2024-08-19 12:24:23 +0530

    fix: stop .git folder being shown in nvim tree on linux

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -10,6 +10,9 @@ return {
       require('nvim-tree').setup {
         view = { side = 'right' },
         update_focused_file = { enable = true },
+        filters = {
+          custom = { '.git' },
+        },
         on_attach = function(bufnr)
           local api = require 'nvim-tree.api'
           -- default mappings

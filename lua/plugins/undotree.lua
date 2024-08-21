f9c4eaed547a0fd0bbff2590a9a3a69c03abb4fc
Afeef Razick <afeefrazickamir@gmail.com>
2024-08-21 21:19:04 +0530

    feat: add undo tree plugin along with keymaps and config

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'mbbill/undotree',
+    config = function()
+      vim.g.undotree_SetFocusWhenToggle = 1
+      vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { noremap = true, silent = true })
+
+      local function remap_directional_keys(normal_key, arrow_key, action)
+        vim.keymap.set('n', normal_key, action, { buffer = 0, noremap = true, silent = true, nowait = true })
+        vim.keymap.set('n', arrow_key, action, { buffer = 0, noremap = true, silent = true, nowait = true })
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('UndotreeMappings', { clear = true }),
+        pattern = 'undotree',
+        callback = function()
+          remap_directional_keys('k', '<up>', '<plug>UndotreeNextState')
+          remap_directional_keys('j', '<down>', '<plug>UndotreePreviousState')
+          remap_directional_keys('h', '<left>', '<plug>UndotreeNextSavedState')
+          remap_directional_keys('l', '<right>', '<plug>UndotreePreviousSavedState')
+        end,
+      })
+    end,
+  },
+}
